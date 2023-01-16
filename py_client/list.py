99cb802e13ca6282acde8706f8dc013fedfa2b5e
Andres Hernandez <edanhebla1213@hotmail.com>
2023-01-15 19:56:03 -0500

    list client

--- py_client/list.py
+++ py_client/list.py
@@ -5,15 +5,16 @@ auth_endpoint = "http://localhost:8000/api/auth/"
 password = getpass()
 
 
-auth_response = requests.post(auth_endpoint, json={'username': 'staff', 'password': password})
+auth_response = requests.post(
+    auth_endpoint, json={'username': 'admin1', 'password': password})
 
 if auth_response.status_code == 200:
-  token = auth_response.json()['token']
-  headers = {
-    'Authorization': f'Bearer {token}'
-  }
-  endpoint = "http://localhost:8000/api/products/"
-  get_response = requests.get(endpoint, headers=headers) # HTTP Request
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    endpoint = "http://localhost:8000/api/products/"
+    get_response = requests.get(endpoint, headers=headers)  # HTTP Request
 
-  print(get_response.json())
-  print(get_response.status_code)
+    print(get_response.json())
+    print(get_response.status_code)
